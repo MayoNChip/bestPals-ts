59a1fc9a19c37f830ad61ac22a71edb8d383508d
Ido Cohen <Idoic44@gmail.com>
2024-02-19 15:48:54 +0200

    types for deployment

--- backend/express.d.ts
+++ backend/express.d.ts
@@ -2,6 +2,11 @@ import { ObjectId } from "mongodb";
 import { IPet } from "./modules/pet/pets.schema";
 import { IUser } from "./modules/user/user.schema";
 
+declare module "express";
+declare module "uuid";
+declare module "bcrypt";
+declare module "jsonwebtoken";
+
 declare global {
 	namespace Express {
 		export interface Request {
