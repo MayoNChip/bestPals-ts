b88ea4fae379c0a56f43e01cb07dd794aa23bd24
Ido Cohen <Idoic44@gmail.com>
2024-03-09 13:48:33 +0200

    cors

--- backend/index.ts
+++ backend/index.ts
@@ -32,10 +32,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(
 	cors({
-		origin: [
-			"https://full-stack-pet-adoption-mayo-n-chip.vercel.app",
-			"http://localhost:5173",
-		],
+		origin: ["http://localhost:5173", "https://best-pals-ts.vercel.app"],
 	})
 );
 
